ba7797ee60d7294896f318c97813e07663f30ac8
Николай Кабаченко <nick.kabachenko@gmail.com>
2025-08-11 17:06:36 +0300

    [Task-17] - Add Icons to tabBar and UserInfoBar

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,12 @@
 import ArrowForwardIcon from './arrow-forward.svg';
 import NotificationBellIcon from './bell.svg';
+import ChatIcon from './chat.svg';
 import EditAvatarIcon from './edit-avatar.svg';
+import HomeIcon from './home-icon.svg';
 import LogoIcon from './logo.svg';
 import PinIcon from './pin-icon.svg';
 import PinIconPinned from './pin-icon-pinned.svg';
+import ProfileIcon from './profile.svg';
 import SearchIcon from './search.svg';
 import SplashRobotIcon from './splash-robot.svg';
 import ToggleFalseIcon from './toggle-false.svg';
@@ -12,11 +15,14 @@ import UserIcon from './user.svg';
 
 export {
   ArrowForwardIcon,
+  ChatIcon,
   EditAvatarIcon,
+  HomeIcon,
   LogoIcon,
   NotificationBellIcon,
   PinIcon,
   PinIconPinned,
+  ProfileIcon,
   SearchIcon,
   SplashRobotIcon,
   ToggleFalseIcon,
