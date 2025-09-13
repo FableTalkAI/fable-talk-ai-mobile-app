bdb271aeb0cb3e6a84d643c809180fe2a2885f76
Николай Кабаченко <nick.kabachenko@gmail.com>
2025-09-13 18:02:37 +0300

    [Task-30] - Add validation

--- src/screens/auth/SignInUpScreen/types.ts
+++ src/screens/auth/SignInUpScreen/types.ts
@@ -3,3 +3,8 @@ import { RouteProp } from '@react-navigation/native';
 import { AuthStackParamList } from '@/navigation/AuthStack/types.ts';
 
 export type SignInUpRouteProp = RouteProp<AuthStackParamList, 'SignInUp'>;
+
+export enum AuthScreenMode {
+  SignIn = 'signIn',
+  SignUp = 'signUp',
+}
