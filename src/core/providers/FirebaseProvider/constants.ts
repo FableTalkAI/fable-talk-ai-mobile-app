0fd985fc6c9f44f7752ab59276ccd31e01bb3252
Nick Kabachenko <109074635+VTUFF@users.noreply.github.com>
2025-10-02 20:01:59 +0300

    [Task-31] - Firebase initialization (#33)
    * [Task-31] - Firebase initialization
    
    * fix: formatter
    
    * fix: ios initialize
    
    ---------
    
    Co-authored-by: mrbywater <nickmelnikbywater@gmail.com>

---
+++ src/core/providers/FirebaseProvider/constants.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyCjpYyqDvGb9tXqU8VzP5Pqf4TNgxB2GzI',
+  authDomain: 'fabletalkai.firebaseapp.com',
+  projectId: 'fabletalkai',
+  storageBucket: 'fabletalkai.firebasestorage.app',
+  messagingSenderId: '434907128053',
+  appId: '1:434907128053:web:e1b397a0bfe3034146a5bc',
+  measurementId: 'G-TRQM6YJ7ZE',
+};
