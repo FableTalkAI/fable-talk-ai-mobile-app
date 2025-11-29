3b15bf85ef1ef20a07a3656dc2afb6384ecbe9dc
Nick Melnik <97626130+mrbywater@users.noreply.github.com>
2025-11-29 20:20:51 +0200

    [Task-42] - Add Sentry (#41)

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,7 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+const { withSentryConfig } = require('@sentry/react-native/metro');
+
 const defaultConfig = getDefaultConfig(__dirname);
 const {
   resolver: { assetExts, sourceExts },
@@ -22,4 +24,4 @@ const config = {
   },
 };
 
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = withSentryConfig(mergeConfig(defaultConfig, config));
