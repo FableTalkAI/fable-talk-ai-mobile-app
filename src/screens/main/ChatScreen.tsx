7599c5230eab15aea5663859d0d7ee5f36976324
mrbywater <nickmelnikbywater@gmail.com>
2026-01-07 19:04:35 +0200

    update/chat-locale

--- src/screens/main/ChatScreen.tsx
+++ src/screens/main/ChatScreen.tsx
@@ -1,14 +1,15 @@
 import dayjs from 'dayjs';
-import uk from 'dayjs/locale/uk';
 import calendar from 'dayjs/plugin/calendar';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { StyleSheet } from 'react-native';
-import { Day, GiftedChat, IMessage } from 'react-native-gifted-chat';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Bubble, Composer, InputToolbar, Message, Send } from '@/components/molecules/giftedChat';
 import ChatAvatar from '@/components/molecules/giftedChat/ChatAvatar.tsx';
 import Header from '@/components/molecules/Header';
+import { DEVICE_LANGUAGE } from '@/core/constants/device.ts';
 import { SPACING } from '@/core/constants/sizes.ts';
 import useChatStore from '@/hooks/useChatStore.ts';
 import useProfileStore from '@/hooks/useProfileStore.ts';
@@ -16,19 +17,35 @@ import useTheme from '@/hooks/useTheme.ts';
 
 dayjs.extend(calendar);
 
-dayjs().calendar(dayjs('2008-01-01'));
-
 const ChatScreen = () => {
   const chatWasUsed = useRef(false);
   const activeSendPromise = useRef<Promise<void> | null>(null);
 
   const { colors } = useTheme();
+  const { t } = useTranslation();
   const insets = useSafeAreaInsets();
 
   const { selectedChat, sendMessageHandler, isLoading, getAllChatsHandler } = useChatStore();
   const { profile } = useProfileStore();
 
   const [messageHistory, setMessageHistory] = useState<IMessage[]>(selectedChat ? selectedChat.messageHistory : []);
+
+  const dateFormatCalendar = {
+    sameDay: t('chat.sameDay'),
+    nextDay: t('chat.nextDay'),
+    nextWeek: 'dddd',
+    lastDay: t('chat.lastDay'),
+    lastWeek: 'dddd',
+    sameElse: 'dddd',
+  };
+
+  const computedStyles = StyleSheet.create({
+    container: {
+      backgroundColor: colors.backgroundTertiary,
+      paddingTop: insets.top > SPACING.m ? 0 : SPACING.m,
+      paddingBottom: insets.bottom > SPACING.m ? 0 : SPACING.m,
+    },
+  });
 
   const onSend = useCallback(
     async (m: IMessage[] = []) => {
@@ -44,14 +61,6 @@ const ChatScreen = () => {
     },
     [sendMessageHandler],
   );
-
-  const computedStyles = StyleSheet.create({
-    container: {
-      backgroundColor: colors.backgroundTertiary,
-      paddingTop: insets.top > SPACING.m ? 0 : SPACING.m,
-      paddingBottom: insets.bottom > SPACING.m ? 0 : SPACING.m,
-    },
-  });
 
   useEffect(() => {
     return () => {
@@ -78,17 +87,10 @@ const ChatScreen = () => {
         onSend={chatMessages => onSend(chatMessages)}
         renderAvatar={props => <ChatAvatar {...props} />}
         //@ts-ignore
-        locale={uk}
-        // isDayAnimationEnabled={true}
-        // dateFormatCalendar={{
-        //   sameDay: '[Today at] h:mm A', // The same day ( Today at 2:30 AM )
-        //   nextDay: '[Tomorrow at] h:mm A', // The next day ( Tomorrow at 2:30 AM )
-        //   nextWeek: 'dddd [at] h:mm A', // The next week ( Sunday at 2:30 AM )
-        //   lastDay: '[Yesterday at] h:mm A', // The day before ( Yesterday at 2:30 AM )
-        //   lastWeek: '[Last] dddd [at] h:mm A', // Last week ( Last Monday at 2:30 AM )
-        //   sameElse: 'DD/MM/YYYY', // Everything else ( 17/10/2011 )
-        // }}
-        dateFormat="DD.MM.YYYY"
+        locale={DEVICE_LANGUAGE}
+        isDayAnimationEnabled={false}
+        dateFormatCalendar={dateFormatCalendar}
+        dateFormat="D MMMM YYYY"
         timeFormat="HH:mm"
         isUserAvatarVisible
         isSendButtonAlwaysVisible
