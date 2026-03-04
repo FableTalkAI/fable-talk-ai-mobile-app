460b5058742c7652637f806ff8e7a6aa0f6a0384
Nick Melnik <97626130+mrbywater@users.noreply.github.com>
2026-03-04 21:39:38 +0200

    [Task-55] - Show BW fix - part2 (#55)

--- src/features/chat/store/chat/selectors.ts
+++ src/features/chat/store/chat/selectors.ts
@@ -3,4 +3,3 @@ import { AppState } from '@/app/store';
 export const isLoadingSelector = (state: AppState) => state.chat.loading;
 export const chatsSelector = (state: AppState) => state.chat.chats;
 export const selectedChatSelector = (state: AppState) => state.chat.selectedChat;
-export const multiSelectionsChatIdsSelector = (state: AppState) => state.chat.multiSelectionsChatIds;
