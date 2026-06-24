e0759ee2c450fdf95f25c0246c3b3794f8eb60e9
Nick Melnik <97626130+mrbywater@users.noreply.github.com>
2026-06-24 21:46:36 +0300

    fix: att request (#83)

--- src/app/providers/AdsProvider/index.tsx
+++ src/app/providers/AdsProvider/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import mobileAds, { AdsConsent, AdsConsentStatus } from 'react-native-google-mobile-ads';
+import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions';
 
 import { AdsProviderProps } from './types.ts';
 
@@ -11,12 +12,15 @@ const AdsProvider = ({ children }: AdsProviderProps) => {
           testDeviceIdentifiers: ['CFA6618D-7F2B-42CE-9CB5-073F9737475D'],
         });
 
-        const consentInfo = await AdsConsent.requestInfoUpdate({
-          debugGeography: __DEV__ ? 1 : 0,
-        });
+        const consentInfo = await AdsConsent.requestInfoUpdate();
 
         if (consentInfo.isConsentFormAvailable && consentInfo.status === AdsConsentStatus.REQUIRED) {
           await AdsConsent.showForm();
+        } else {
+          const result = await check(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY);
+          if (result === RESULTS.DENIED) {
+            await request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY);
+          }
         }
 
         await mobileAds().initialize();
